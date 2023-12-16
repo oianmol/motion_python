e2ddfe6aa98b555e66743c66d0b9298dda00543d
Anmol Verma <anmol.verma4@gmail.com>
2023-12-16 14:55:37 +0530

    add: video writer for 5 mins

--- CameraMotion.py
+++ CameraMotion.py
@@ -75,14 +75,15 @@ class CameraMotion:
                 while self.video_stream.more():
                     try:
                         original_frame = self.video_stream.read()
+                        self.motion_file_processor.take(original_frame=original_frame)
+
                         if self.output_motion_video:
                             if self.video_start_time is not None:
                                 diff_time = datetime.now() - self.video_start_time
-                                if diff_time >= timedelta(minutes=5):
+                                if diff_time >= timedelta(seconds=4):
                                     self.video_writer.release()
                                     self.video_writer = None
                                     self.video_start_time = None
-                                    self.motion_file_processor.take(video_file_path=self.video_file_output)
 
                             if self.video_writer is None:
                                 self.video_start_time = datetime.now()

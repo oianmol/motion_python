c0b4ec05c90a451ceb3c41830c0539c9eaa5548e
Anmol Verma <anmol.verma4@gmail.com>
2023-12-16 10:03:52 +0530

    try with MOG2

--- MOG2.py
+++ MOG2.py
@@ -2,6 +2,8 @@ import cv2
 
 
 def create(parser):
-    mog2 = cv2.bgsegm.createBackgroundSubtractorCNT()
+    mog2 = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
     detect_shadows = parser.getboolean("DEFAULT", "detect_shadows")
+    if bool(detect_shadows):
+        mog2.setShadowValue(0)
     return mog2
